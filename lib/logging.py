76646bf781e68437a6981d80c24f9d87fd05bbf4
Jaakko Kantojärvi <jaakko@cs.hut.fi>
2016-12-31 07:45:49 +0200

    Add colorizing logger

--- lib/logging.py
+++ lib/logging.py
@@ -1,4 +1,9 @@
+import logging
+from copy import copy
+from django.core.management.color import color_style
 from django.http import UnreadablePostError
+from django.utils.termcolors import make_style
+
 
 def skip_unreadable_post(record):
     """ Skips log records of unfinished post requests. """
@@ -7,3 +12,34 @@ def skip_unreadable_post(record):
         if isinstance(exc_value, UnreadablePostError):
             return False
     return True
+
+
+class SourceColorizeFormatter(logging.Formatter):
+    def __init__(self, *args, **kwargs):
+        color_conf = kwargs.pop('colors', None)
+        super().__init__(*args, **kwargs)
+        style = color_style()
+        self.mapping = self.create_mapping(style, color_conf)
+
+    def create_mapping(self, style, conf):
+        mapping = {}
+        for name, val in conf.items():
+            if isinstance(val, dict):
+                style_func = make_style(**val)
+            else:
+                style_func = getattr(style, val, None)
+
+            if style_func:
+                mapping[name] = style_func
+
+        return mapping
+
+    def format(self, record):
+        name = record.name
+        possible = [k for k in self.mapping.keys() if k.startswith(name)]
+        if possible:
+            record = copy(record)
+            key = min(possible, key=lambda x: len(x))
+            colorizer = self.mapping[key]
+            record.msg = colorizer(record.msg)
+        return super().format(record)
